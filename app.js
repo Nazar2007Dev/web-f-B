8e530a4c5ca205c3a3b6dec896cd96ae57fd24ce
Nazar2007dev <maulidannazar4@gmail.com>
2024-09-19 15:02:11 +0700

    gg

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("products", () => ({
     items: [
-      { id: 1, name: "Croissant", img: "1.jpg", price: 10000 },
+      { id: 1, name: "Lemon juice", img: "1.jpg", price: 15000 },
       { id: 2, name: "Creme Brulee", img: "2.jpg", price: 15000 },
-      { id: 3, name: "Macchiato", img: "3.jpg", price: 14000 }
+      { id: 3, name: "Blue lagoon", img: "3.jpg", price: 14000 },
+      { id: 4, name: "Chicken Flatter", img: "4.jpg", price: 15000 },
+      { id: 5, name: "Macaroni Schotel", img: "5.jpg", price: 10000 },
+      { id: 6, name: "Lychee Tea", img: "6.jpg", price: 15000 },
+      { id: 7, name: "Bread", img: "7.jpg", price: 5000 },
+      { id: 8, name: "Cocktail", img: "8.jpg", price: 10000 }
     ]
   }));
 
